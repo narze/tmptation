252f9702a91b923c9ed86124031e4767cb341d68
Manassarn Manoonchai <manassarn@gmail.com>
2023-05-26 00:38:52 +0700

    Simple homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,21 @@
 "use client"
 
-import "@uploadthing/react/styles.css"
+// import "@uploadthing/react/styles.css"
 
-import { UploadButton } from "@uploadthing/react"
-import { OurFileRouter } from "@/app/api/uploadthing/core"
+// import { UploadButton } from "@uploadthing/react"
+// import { OurFileRouter } from "@/app/api/uploadthing/core"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UploadButton<OurFileRouter>
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          // Do something with the response
-          console.log("Files: ", res)
-          alert("Upload Completed")
-        }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`)
-        }}
-      />
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <code>
+        curl -F &quot;file=@/path/to/file&quot;
+        &quot;https://tmptation.vercel.app/api/upload&quot;
+      </code>
+
+      <div className="flex flex-col items-center justify-center">
+        File cannot be too big or it will fail.
+      </div>
     </main>
   )
 }
